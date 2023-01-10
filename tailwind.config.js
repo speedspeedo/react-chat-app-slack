305f9a11cecf0f8fb015054d770b689614c36fc2
speedo1004 <speedodeveloper1004@gmail.com>
2023-01-10 14:04:59 +0900

    Fourth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    // 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
